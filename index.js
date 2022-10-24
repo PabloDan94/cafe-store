49d9f8a5da776cb1aa06275e6fc8ef1741e32eb1
Pablo <pablo.iriarte.94@hotmail.com>
2022-10-24 17:45:06 -0300

    arreglos

--- index.js
+++ index.js
@@ -20,17 +20,3 @@ ul.addEventListener('click', e => {
     nav.classList.toggle('nav')
 
 })
-
-
-window.onscroll = () =>{
-
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-  
-    if(window.scrollY > 60){
-      document.querySelector('#scroll-top').classList.add('active');
-    }else{
-      document.querySelector('#scroll-top').classList.remove('active');
-    }
-  
-  }
